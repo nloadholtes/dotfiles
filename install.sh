67e7231833749765b3511ce835db00b36bf40e61
Nick Loadholtes nick <Nick Loadholtes nick@ironboundsoftware.com>
2012-12-16 16:02:26 -0500

    Updating the install file to actually do something

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-TODO: Make this work.
+cd
+ln -s projects/bitbucket/dotfiles/hg/.hgrc .hgrc
+ln -s projects/bitbucket/dotfiles/hg/.hgignore .hgignore
+ln -s projects/bitbucket/dotfiles/git/.gitconfig .gitconfig
+ln -s projects/bitbucket/dotfiles/git/.gitignore .gitignore
+ln -s projects/bitbucket/dotfiles/emacs/.emacs .emacs
+ln -s projects/bitbucket/dotfiles/emacs/emacs.d/ .emacs.d
+ln -s projects/bitbucket/dotfiles/unix/.pylintrc .pylintrc
+ln -s projects/bitbucket/dotfiles/unix/alias .alias
+
