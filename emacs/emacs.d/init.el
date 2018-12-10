aa7ec6e6e7a2d61fef3f6e3957a93183df5fa4e6
Nick Loadholtes <nick@ironboundsoftware.com>
2018-12-09 20:19:33 -0500

    Formatting

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -57,18 +57,13 @@
 
 ;; colors and keys
 (custom-set-faces
- '(font-lock-comment-face ((((class color) 
-                             (min-colors 88) 
-                             (background dark)) 
-                            (:foreground "yellow" :weight bold))))
- '(font-lock-function-name-face ((((class color) 
-                                   (min-colors 88) 
-                                   (background dark)) 
-                                  (:foreground "SkyBlue"))))
- '(font-lock-keyword-face ((((class color) 
-                             (min-colors 88) 
-                             (background dark)) 
-                            (:foreground "Cyan" :weight bold))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background dark)) (:foreground "yellow" :weight bold))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background dark)) (:foreground "SkyBlue"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background dark)) (:foreground "Cyan" :weight bold))))
  '(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:foreground "green")))))
 
 (global-set-key (kbd "C-z") 'undo)
