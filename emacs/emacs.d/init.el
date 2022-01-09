a11fb2da71188c70aad8d587eb555b6a32190201
Nick Loadholtes <nick.loadholtes@datarobot.com>
2022-01-09 15:20:02 -0500

    Smaller font, changing from mono to try it out

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -228,4 +228,4 @@ See `markdown-insert-header'."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Ubuntu" :foundry "DAMA" :slant normal :weight normal :height 109 :width normal)))))
