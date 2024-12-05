1358725ce9fe9e0a36e034d19dc418cff30c2c2d
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-05 11:05:04 -0500

    My attempt to get my lazyvim/nvim configuration under control (the autocomplete was going nuts, now I am trying to track everything to make sure I'm able to roll back in the event something goes wrong. For example, see my emacs config

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,9 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    completion = {
+      keyword_length = 3, -- require 2 characters before showing completions
+    },
+    preselect = "none", -- don't preselect first item
+  },
+}
---
+++ nvim/lua/plugins/nvim-snippets.lua
@@ -0,0 +1,8 @@
+return {
+  "friendly-snippets",
+  opts = {
+    expand_conditions = {
+      min_characters = 4, -- require 4 chars before expansion
+    },
+  },
+}
