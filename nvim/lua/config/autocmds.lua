8dd23135994c4bb399bfa61dff89680d1dd756b4
Nick Loadholtes <nick@ironboundsoftware.com>
2025-07-09 09:54:59 -0400

    auto trim whitespace from the end of the line on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+
+-- Auto-trim trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	command = ":%s/\\s\\+$//e",
+	desc = "Trim trailing whitespace on save",
+})
