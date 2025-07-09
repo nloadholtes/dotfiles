35607370ddc02b0f1cfd1199bdce27296dc82167
Nick Loadholtes <nick@ironboundsoftware.com>
2025-07-09 15:13:55 -0400

    Apprently the completion thing changed...

--- nvim/lua/plugins/markdown_autocomplete_off.lua
+++ nvim/lua/plugins/markdown_autocomplete_off.lua
@@ -1,15 +1,12 @@
 -- This is to help turn off the auto-complete in markdown files.
 
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    init = function()
-      vim.g.cmp_disabled = false
-    end,
-    opts = {
-      enabled = function()
-        return (vim.bo.ft ~= "markdown")
-      end,
-    },
-  },
+	{
+		"saghen/blink.cmp",
+		opts = {
+			enabled = function()
+				return vim.bo.filetype ~= "markdown"
+			end,
+		},
+	},
 }
