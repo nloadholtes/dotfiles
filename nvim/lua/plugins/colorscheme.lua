784b7f67774f6eb0b8187de445565a2a5ac7ce1f
Nick Loadholtes <nick@ironboundsoftware.com>
2025-09-29 12:40:07 -0400

    A new color scheme in nvim, ignore obsidian

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,21 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  { "shaunsingh/nord.nvim" },
-  { "rmehri01/onenord.nvim" },
-  { "ribru17/bamboo.nvim" },
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "bamboo",
-    },
-  },
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
+	{ "shaunsingh/nord.nvim" },
+	{ "rmehri01/onenord.nvim" },
+	{ "ribru17/bamboo.nvim" },
+	{
+		"sainnhe/sonokai",
+		config = function()
+			-- Set the global variable g:sonokai_style *before* loading the colorscheme
+			vim.g.sonokai_style = "andromeda"
+		end,
+	},
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "sonokai",
+		},
+	},
 }
